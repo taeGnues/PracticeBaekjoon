17ec87c7840326f5e80e73c148f1185e0ae9465d
정태승 <112752089+taeGnues@users.noreply.github.com>
2023-04-28 21:51:06 +0900

    [Gold IV] Title: 알파벳, Time: 416 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int R, C, res;
-int bd[25][25], vis[25][25];
+int bd[25][25];
 int useAlph[26];
 
 
@@ -11,7 +11,7 @@ int dy[4] = {0, -1, 0, 1};
 int dx[4] = {1, 0, -1, 0};
 
 int dfs(int y, int x, int level){
-    vis[y][x] = 1; // 방문
+    
     useAlph[bd[y][x]] = 1; // 해당 알파뱃 사용 체크.
     
     int ret = level;
@@ -20,12 +20,11 @@ int dfs(int y, int x, int level){
         int ny = y + dy[i];
         int nx = x + dx[i];
         
-        if(ny > R || nx > C || ny <= 0 || nx <= 0 || vis[ny][nx]) continue;
+        if(ny > R || nx > C || ny <= 0 || nx <= 0) continue;
         if(useAlph[bd[ny][nx]]) continue; // 해당 알파벳이 사용됐다면 무시.
         
         ret = max(ret, dfs(ny, nx, level+1));
         
-        vis[ny][nx] = 0;
         useAlph[bd[ny][nx]] = 0; // 원래대로 돌려주기
         
     }
