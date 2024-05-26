c10d17bdd6401b4eccbecb98054a1685f88ba4d8
정태승 <112752089+taeGnues@users.noreply.github.com>
2024-05-26 11:33:29 +0900

    [Gold V] Title: 도로의 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1577. 도로의 개수/도로의 개수.py
+++ 백준/Gold/1577. 도로의 개수/도로의 개수.py
@@ -5,36 +5,19 @@ N, M = map(int, sys.stdin.readline().split())
 K = int(sys.stdin.readline())
 
 blocked = {}
-dp = {}
 for i in range(K):
     a, b, c, d = map(int, sys.stdin.readline().split()) # (a,b), (c,d)
-    blocked[(c,d,a,b)] = True
-    blocked[(a,b,c,d)] = True
+    blocked[(c, d, a, b)] = True
+    blocked[(a, b, c, d)] = True
 
+dp = [[0] * (M + 1) for _ in range(N + 1)]
+dp[0][0] = 1
 
-def go(cx, cy, level):
+for i in range(N + 1):
+    for j in range(M + 1):
+        if i < N and (i, j, i + 1, j) not in blocked:
+            dp[i + 1][j] += dp[i][j]
+        if j < M and (i, j, i, j + 1) not in blocked:
+            dp[i][j + 1] += dp[i][j]
 
-    if cx == N and cy == M and level == N+M:
-        return 1
-    
-    if level >= N+M or cx > N or cy > M :
-        return 0
-    
-    if (cx, cy) in dp:
-        return dp[(cx, cy)]
-
-    dp[(cx, cy)] = 0
-    for d in [(1,0), (0,1)]:
-        nx = cx + d[0]
-        ny = cy + d[1]
-
-        if (cx, cy, nx, ny) in blocked:
-            continue
-        
-        dp[(cx, cy)] += go(nx, ny, level+1)
-    
-    return dp[(cx, cy)]
-
-
-
-print(go(0,0,0))
+print(dp[N][M])
