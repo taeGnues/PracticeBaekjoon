dbd0c83e85e263d0d547b3aefe05cd885f05c146
정태승 <112752089+taeGnues@users.noreply.github.com>
2024-07-16 16:47:27 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 911.32 ms, Memory: 143 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -0,0 +1,27 @@
+def solution(board, skill):
+    answer = 0
+    tmp=[[0]*(len(board[0])+1) for _ in range(len(board)+1)]# 누적합 기록을 위한 배열
+    
+    for t, r1, c1, r2, c2, degree in skill:
+        tmp[r1][c1]+=degree if t == 2 else -degree
+        tmp[r1][c2+1]+=-degree if t == 2 else degree
+        tmp[r2+1][c1]+=-degree if t == 2 else degree
+        tmp[r2+1][c2+1]+=degree if t == 2 else -degree
+        
+    # 행 기준 누적합
+    for i in range(len(tmp)-1):
+        for j in range(len(tmp[0])-1):
+            tmp[i][j+1]+= tmp[i][j]
+
+    # 열 기준 누적합
+    for j in range(len(tmp[0])-1):
+        for i in range(len(tmp)-1):
+            tmp[i+1][j]  += tmp[i][j]
+            
+    # 기존 배열과 합함
+    for i in range(len(board)):
+          for j in range(len(board[i])):
+                board[i][j]+=tmp[i][j]
+                if board[i][j] > 0:
+                      answer+=1
+    return answer
